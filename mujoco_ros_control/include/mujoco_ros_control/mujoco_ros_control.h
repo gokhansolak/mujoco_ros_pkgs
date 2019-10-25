2aebd7c661d039a9fb91c26b85648d5af2a3c1fd
Gokhan Solak <gknsolak@gmail.com>
2019-10-25 16:30:43 +0900

    mujoco path (adopting mujoco-py way)

--- mujoco_ros_control/include/mujoco_ros_control/mujoco_ros_control.h
+++ mujoco_ros_control/include/mujoco_ros_control/mujoco_ros_control.h
@@ -123,7 +123,7 @@ protected:
   std::string robot_namespace_;
   std::string robot_description_param_;
   std::string robot_model_path_;
-  std::string key_path_ = "/home/user/mjpro150/bin/mjkey.txt";
+  std::string key_path_ = "~/.mujoco/mujoco200/bin/mjkey.txt";
 
   // vectors
   std::vector<int> mujoco_ids;
